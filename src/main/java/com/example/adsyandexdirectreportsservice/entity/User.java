95d228440d833d8c90f176fee2964bc2c7060cf2
Pavel Isaenko <pokedbrain@gmail.com>
2025-08-22 21:02:06 +1000

    initial works

--- src/main/java/com/example/adsyandexdirectreportsservice/entity/User.java
+++ src/main/java/com/example/adsyandexdirectreportsservice/entity/User.java
@@ -1,15 +1,22 @@
 package com.example.adsyandexdirectreportsservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 
+@Entity
+@Data
 public class User implements GrantedAuthority {
 
+    @Id
+    private Long id;
     private String username;
     private String email;
-    private
 
     @Override
     public String getAuthority() {
         return "";
     }
+
 }
